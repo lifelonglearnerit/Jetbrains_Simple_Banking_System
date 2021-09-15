ace0924cff3b924dfb87bc182912e2ec987dcb8d
lifelonglearnerit <lifelonglearnerit@gmail.com>
2021-09-15 07:29:24 +0200

    added check_check: verification of card number and pin in sql table.

--- banking.py
+++ banking.py
@@ -3,15 +3,28 @@
 # In our banking system, account number should be unique.
 # And the whole card number should be 16-digit length.
 import random
+import sqlite3
 
 
 class BankAccount:
-    accounts_list = []
 
+    # database creation
+    db = sqlite3.connect('card.s3db')
+    cur = db.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS card ('
+                '   id INT,'
+                '   number VARCHAR(16),'
+                '   pin VARCHAR(4),'
+                '   balance INT DEFAULT 0'
+                '     );')
+
+    # list of accounts will be not needed since we have db
+    # accounts_list = []
     def __init__(self):
         self.card_number = '4000000000000000'
-        self.pin = '0000'
+        self.pin_number = '0000'
         self.main_menu()
+
 
     def main_menu(self):
         actions = {'1': self.create_account,
@@ -22,7 +35,7 @@ class BankAccount:
                             '2. Log into account\n'
                             '0. Exit\n')
         if menu_action == '1':
-            actions['1'](self.card_number, self.pin)
+            actions['1'](self.card_number, self.pin_number)
         elif menu_action == '2':
             actions['2']()
         elif menu_action == '0':
@@ -39,28 +52,36 @@ class BankAccount:
 
         return str(ch_sum)
 
-    def create_account(self, card_number, pin):
-
-        self.accounts_list.append(self.card_generator(self.card_number, self.pin))
-        # while True:
-        #     self.card_generator(self.card_number, self.pin)
-        #     if self.card_generator(self.card_number, self.pin) in self.accounts_list:
-        #         self.card_generator(self.card_number, self.pin)
-        #     else:
-        #
-        #         self.accounts_list.append(self.card_generator(self.card_number, self.pin))
-        #         break
+    def create_account(self, card_number, pin_number):
+        gen_card_number = self.card_generator(card_number)
+        gen_pin_number = self.pin_generator(pin_number)
+        #self.accounts_list.append(self.card_generator(self.card_number, self.pin))
+        self.cur.execute(f'INSERT INTO card (number, pin) VALUES ({gen_card_number}, {gen_pin_number});')
+        self.db.commit()
+        self.cur.execute(f'SELECT * FROM card;')
 
         print('Your card has been created')
-        if len(self.accounts_list) >= 1:
-            print(f'Your card number:\n{self.accounts_list[len(self.accounts_list) - 1][0]}')
-        else:
-            print(f'Your card number:\n{self.accounts_list[0][0]}')
-        print(f'Your card PIN:\n{self.accounts_list[len(self.accounts_list) - 1][1]}')
+        print(f'Your card number:\n{gen_card_number}')
+        print(f'Your card pin:\n{gen_pin_number}')
+        # below code is just for debugging
+        print(self.cur.fetchall())
+        #self.cur.execute(f'DELETE FROM card;')
+        #self.db.commit()
+        #print(self.cur.fetchall())
+
+        # CHANGE IT FOR QUERY TO COMPARE EXISTING NUMBER WITH GENERATED - REGEX?
+        # if len(self.accounts_list) >= 1:
+        #     print(f'Your card number:\n{self.accounts_list[len(self.accounts_list) - 1][0]}')
+        # else:
+        #     print(f'Your card number:\n{self.accounts_list[0][0]}')
+
+        # self.cur.execute(f'INSERT INTO card (pin) VALUES ({self.pin_generator(pin)});')
+        # self.db.commit()
+        #print(f'Your card PIN:\n{self.accounts_list[len(self.accounts_list) - 1][1]}')
         # print(self.accounts_list)
         self.main_menu()
 
-    def card_generator(self, card_number, pin):
+    def card_generator(self, card_number):
         for i in range(6, 15):
             self.card_number = list(self.card_number)
             self.card_number[i] = str(random.randint(0, 8))
@@ -68,16 +89,28 @@ class BankAccount:
         check_sum = self.luhn_check_sum(self.card_number)
         self.card_number.append(check_sum)
         self.card_number = "".join(self.card_number)
+        return self.card_number
 
+
+    # NEW METHOD FOR PIN
+    def pin_generator(self, pin_number):
         for x in range(4):
-            self.pin = list(self.pin)
-            self.pin[x] = str(random.randint(0, 9))
-            self.pin = "".join(self.pin)
-        return self.card_number, self.pin
+            self.pin_number = list(self.pin_number)
+            self.pin_number[x] = str(random.randint(0, 9))
+        self.pin_number = "".join(self.pin_number)
+        return self.pin_number
+
+    def card_check(self, _card_number, _pin_number):
+        self.cur.execute(f'SELECT number, pin FROM card  WHERE number = "{_card_number}" AND pin = "{_pin_number}";')
+        result = self.cur.fetchall()
+        if len(result) == 0:
+            return False
+        else:
+            return True
 
     def log_to_account(self):
 
-        if (input('Enter your card number:\n'), input('Enter your PIN:\n')) in self.accounts_list:
+        if self.card_check(input('Enter your card number:\n'), input('Enter your PIN:\n')):
             print('You have successfully logged in!')
             self.logged_menu()
         else:
@@ -98,6 +131,9 @@ class BankAccount:
             logged_actions['2']()
         elif menu_logged == '0':
             logged_actions['0']()
+
+
+
 
     def account_balance(self):
         print('Balance: 0')
